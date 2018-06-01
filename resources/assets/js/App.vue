b1958e6fca945466fe08fdd8c96f59ac54433a08
Gerardo Belot <gbelot2003@hotmail.com>
2018-06-01 17:34:34 -0600

    Configurando estructura de router

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -1,26 +1,6 @@
 <template>
     <v-app id="inspire">
-        <v-navigation-drawer
-                v-model="drawer" fixed app >
-            <v-list dense>
-                <v-list-tile @click="">
-                    <v-list-tile-action>
-                        <v-icon>home</v-icon>
-                    </v-list-tile-action>
-                    <v-list-tile-content>
-                        <v-list-tile-title>Home</v-list-tile-title>
-                    </v-list-tile-content>
-                </v-list-tile>
-                <v-list-tile @click="">
-                    <v-list-tile-action>
-                        <v-icon>contact_mail</v-icon>
-                    </v-list-tile-action>
-                    <v-list-tile-content>
-                        <v-list-tile-title>Contact</v-list-tile-title>
-                    </v-list-tile-content>
-                </v-list-tile>
-            </v-list>
-        </v-navigation-drawer>
+
         <v-toolbar color="indigo" dark fixed app>
             <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
             <v-toolbar-title>Application</v-toolbar-title>
@@ -35,6 +15,7 @@
 </template>
 
 <script>
+    import NavBar from './components/base/Navbar.vue';
     export default {
         data: () => ({
             drawer: null
